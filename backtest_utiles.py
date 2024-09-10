d1ea902395842f1a616b6c5c0641b90a5d34e1d0
SHEN Yifan <yifan.shen.17@gmail.com>
2024-09-10 01:09:02 +0100

    Add rescale spread function

--- backtest_utiles.py
+++ backtest_utiles.py
@@ -1,10 +1,13 @@
 '''''
 Utile functions for mean reversion pair trading strategy
 '''
+from pyexpat.errors import XML_ERROR_UNCLOSED_TOKEN
+from statistics import linear_regression
 import pandas as pd 
 import numpy as np 
 from itertools import combinations
 from statsmodels.tsa.stattools import coint
+import statsmodels.api as sm
 
 def select_stock_pairs_from_correlation(df_period: pd.DataFrame, 
                                         top_corr_pairs: int=15,
@@ -29,7 +32,7 @@ def select_stock_pairs_from_correlation(df_period: pd.DataFrame,
 # Function to find cointegrated pairs
 def find_cointegrated_pairs(df_period: pd.DataFrame,
                             coint_test_list: list, 
-                            p_value_threshold: float=0.01):
+                            p_value_threshold: float=0.01) -> list:
     ''''
     Give a list of tickers to test, and p_value_threshold 
     Find cointegrated pairs using Engle-Granger test
@@ -46,6 +49,26 @@ def find_cointegrated_pairs(df_period: pd.DataFrame,
     coint_pairs = [(coint_test_list[i], coint_test_list[j], pvalue_matrix[i,j]) for i in range(n) for j in range(i+1, n) if pvalue_matrix[i, j] < p_value_threshold]
     return coint_pairs  # Return the p-value matrix and cointegrated pairs
 
+def rescale_price_between_selected_pairs(df_period: pd.DataFrame, 
+                                         ticker1: str, 
+                                         ticker2: str):
+    ''''
+    Linear regression on two conintegrated stocks, then scale spread
+    '''
+    # ticker1
+    x = np.log(df_period[ticker1].values)
+    x_const = sm.add_constant(x)
+    # ticker2
+    y = np.log(df_period[ticker2].values)
+    linear_reg = sm.OLS(y, x_const)
+    result = linear_reg.fit()
+    alpha = result.params[0]
+    beta = result.params[1]
+    df_spread = pd.DataFrame(np.log(df_period[ticker2]) - np.log(df_period[ticker1])*beta-alpha)
+
+    
+    pass
+    
 
 if __name__ == '__main__':
     df = pd.read_csv('SPX_components_close_data.csv').set_index('Date')
